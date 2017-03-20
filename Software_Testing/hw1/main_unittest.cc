ba5c4a86f0f1acbd708d998e65246631ac2aa47f
bananaappletw <bananaappletw@gmail.com>
2017-03-21 03:21:53 +0800

    Add weak equivalence class test

--- Software_Testing/hw1/main_unittest.cc
+++ Software_Testing/hw1/main_unittest.cc
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "main.h"
+
 TEST(triangle_type, BoundaryValue){
 	EXPECT_STREQ("Isosceles", triangle_type(100, 100, 1));
 	EXPECT_STREQ("Isosceles", triangle_type(100, 100, 2));
@@ -14,4 +15,14 @@ TEST(triangle_type, BoundaryValue){
 	EXPECT_STREQ("Isosceles", triangle_type(2, 100, 100));
 	EXPECT_STREQ("Isosceles", triangle_type(199, 100, 100));
 	EXPECT_STREQ("Not a Triangle", triangle_type(200, 100, 100));
+}
+
+TEST(triangle_type, EquivalenceClass){
+  EXPECT_STREQ("Equilateral", triangle_type(5, 5, 5));
+  EXPECT_STREQ("Invalid", triangle_type(-1 , 5, 5));
+  EXPECT_STREQ("Invalid", triangle_type(5 , -1, 5));
+  EXPECT_STREQ("Invalid", triangle_type(5 , 5, -1));
+  EXPECT_STREQ("Invalid", triangle_type(201 , 5, 5));
+  EXPECT_STREQ("Invalid", triangle_type(5 , 201, 5));
+  EXPECT_STREQ("Invalid", triangle_type(5 , 5, 201));
 }
