67b75cf79d08e8ba00a8cf860766612a6a6fb68b
bananaappletw <bananaappletw@gmail.com>
2017-03-21 04:16:58 +0800

    Format code

--- Software_Testing/hw1/main.cc
+++ Software_Testing/hw1/main.cc
@@ -4,33 +4,33 @@
 
 const char* triangle_type(int a, int b, int c)
 {
-	if ( 1 > a || a > 200 )
-		return "Invalid";
+    if ( 1 > a || a > 200 )
+        return "Invalid";
 
-	if ( 1 > b || b > 200 )
-		return "Invalid";
+    if ( 1 > b || b > 200 )
+        return "Invalid";
 
-	if ( 1 > c || c > 200 )
-		return "Invalid";
+    if ( 1 > c || c > 200 )
+        return "Invalid";
 
-	int tmp;
+    int tmp;
 
-	if ( a > b )
-		SWAP(a,b,tmp);
-	if ( b > c )
-		SWAP(b,c,tmp);
-	if ( a > b )
-		SWAP(a,b,tmp);
+    if ( a > b )
+        SWAP(a,b,tmp);
+    if ( b > c )
+        SWAP(b,c,tmp);
+    if ( a > b )
+        SWAP(a,b,tmp);
 
-	if ( a + b <= c )
-		return "Not a Triangle";
+    if ( a + b <= c )
+        return "Not a Triangle";
 
-	if( a == b && b == c )
-		return "Equilateral";
-	if( a == b && a != c && b != c )
-		return "Isosceles";
-	else if( b == c && b != a && c != a )
-		return "Isosceles";
-	else
-		return "Scalene";
+    if( a == b && b == c )
+        return "Equilateral";
+    if( a == b && a != c && b != c )
+        return "Isosceles";
+    else if( b == c && b != a && c != a )
+        return "Isosceles";
+    else
+        return "Scalene";
 }
--- Software_Testing/hw1/main_unittest.cc
+++ Software_Testing/hw1/main_unittest.cc
@@ -1,49 +1,49 @@
 #include "gtest/gtest.h"
 #include "main.h"
 
-TEST(triangle_type, BoundaryValue){
-	EXPECT_STREQ("Isosceles", triangle_type(100, 100, 1));
-	EXPECT_STREQ("Isosceles", triangle_type(100, 100, 2));
-	EXPECT_STREQ("Equilateral", triangle_type(100, 100, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(100, 100, 199));
-	EXPECT_STREQ("Not a Triangle", triangle_type(100, 100, 200));
-	EXPECT_STREQ("Isosceles", triangle_type(100, 1, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(100, 2, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(100, 199, 100));
-	EXPECT_STREQ("Not a Triangle", triangle_type(100, 200, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(1, 100, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(2, 100, 100));
-	EXPECT_STREQ("Isosceles", triangle_type(199, 100, 100));
-	EXPECT_STREQ("Not a Triangle", triangle_type(200, 100, 100));
+TEST(triangle_type, BoundaryValue) {
+    EXPECT_STREQ("Isosceles", triangle_type(100, 100, 1));
+    EXPECT_STREQ("Isosceles", triangle_type(100, 100, 2));
+    EXPECT_STREQ("Equilateral", triangle_type(100, 100, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(100, 100, 199));
+    EXPECT_STREQ("Not a Triangle", triangle_type(100, 100, 200));
+    EXPECT_STREQ("Isosceles", triangle_type(100, 1, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(100, 2, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(100, 199, 100));
+    EXPECT_STREQ("Not a Triangle", triangle_type(100, 200, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(1, 100, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(2, 100, 100));
+    EXPECT_STREQ("Isosceles", triangle_type(199, 100, 100));
+    EXPECT_STREQ("Not a Triangle", triangle_type(200, 100, 100));
 }
 
-TEST(triangle_type, EquivalenceClass){
-  EXPECT_STREQ("Equilateral", triangle_type(5, 5, 5));
-  EXPECT_STREQ("Invalid", triangle_type(-1 , 5, 5));
-  EXPECT_STREQ("Invalid", triangle_type(5 , -1, 5));
-  EXPECT_STREQ("Invalid", triangle_type(5 , 5, -1));
-  EXPECT_STREQ("Invalid", triangle_type(201 , 5, 5));
-  EXPECT_STREQ("Invalid", triangle_type(5 , 201, 5));
-  EXPECT_STREQ("Invalid", triangle_type(5 , 5, 201));
+TEST(triangle_type, EquivalenceClass) {
+    EXPECT_STREQ("Equilateral", triangle_type(5, 5, 5));
+    EXPECT_STREQ("Invalid", triangle_type(-1 , 5, 5));
+    EXPECT_STREQ("Invalid", triangle_type(5 , -1, 5));
+    EXPECT_STREQ("Invalid", triangle_type(5 , 5, -1));
+    EXPECT_STREQ("Invalid", triangle_type(201 , 5, 5));
+    EXPECT_STREQ("Invalid", triangle_type(5 , 201, 5));
+    EXPECT_STREQ("Invalid", triangle_type(5 , 5, 201));
 }
 
-TEST(triangle_type, Edge){
-  EXPECT_STREQ("Invalid", triangle_type(0, 5, 5));
-  EXPECT_STREQ("Isosceles", triangle_type(1 , 5, 5));
-  EXPECT_STREQ("Isosceles", triangle_type(2 , 5, 5));
-  EXPECT_STREQ("Not a Triangle", triangle_type(199 , 5, 5));
-  EXPECT_STREQ("Not a Triangle", triangle_type(200 , 5, 5));
-  EXPECT_STREQ("Invalid", triangle_type(201, 5, 5));
+TEST(triangle_type, Edge) {
+    EXPECT_STREQ("Invalid", triangle_type(0, 5, 5));
+    EXPECT_STREQ("Isosceles", triangle_type(1 , 5, 5));
+    EXPECT_STREQ("Isosceles", triangle_type(2 , 5, 5));
+    EXPECT_STREQ("Not a Triangle", triangle_type(199 , 5, 5));
+    EXPECT_STREQ("Not a Triangle", triangle_type(200 , 5, 5));
+    EXPECT_STREQ("Invalid", triangle_type(201, 5, 5));
 }
 
-TEST(triangle_type, DecisionTable){
-  EXPECT_STREQ("Not a Triangle", triangle_type(4, 1, 2));
-  EXPECT_STREQ("Not a Triangle", triangle_type(1, 4, 2));
-  EXPECT_STREQ("Not a Triangle", triangle_type(1, 2, 4));
-  EXPECT_STREQ("Equilateral", triangle_type(5, 5, 5));
-  EXPECT_STREQ("Isosceles", triangle_type(2 , 2, 3));
-  EXPECT_STREQ("Isosceles", triangle_type(2 , 3, 2));
-  EXPECT_STREQ("Isosceles", triangle_type(3 , 2, 2));
-  EXPECT_STREQ("Isosceles", triangle_type(2 , 3, 2));
-  EXPECT_STREQ("Scalene", triangle_type(3 , 4, 5));
+TEST(triangle_type, DecisionTable) {
+    EXPECT_STREQ("Not a Triangle", triangle_type(4, 1, 2));
+    EXPECT_STREQ("Not a Triangle", triangle_type(1, 4, 2));
+    EXPECT_STREQ("Not a Triangle", triangle_type(1, 2, 4));
+    EXPECT_STREQ("Equilateral", triangle_type(5, 5, 5));
+    EXPECT_STREQ("Isosceles", triangle_type(2 , 2, 3));
+    EXPECT_STREQ("Isosceles", triangle_type(2 , 3, 2));
+    EXPECT_STREQ("Isosceles", triangle_type(3 , 2, 2));
+    EXPECT_STREQ("Isosceles", triangle_type(2 , 3, 2));
+    EXPECT_STREQ("Scalene", triangle_type(3 , 4, 5));
 }
