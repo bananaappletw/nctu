1ef5dcbb3cb2a66d003a6a63c5c87a02fbb3eb48
bananaappletw <bananaappletw@gmail.com>
2017-05-30 02:31:56 -0700

    Add hw4

---
+++ Software_Testing/hw4/main.cc
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+
+const char* triangle_type(int a, int b, int c)
+{
+    if ( 1 > a || a > 200 )
+        return "Invalid";
+
+    if ( 1 > b || b > 200 )
+        return "Invalid";
+
+    if ( 1 > c || c > 200 )
+        return "Invalid";
+
+    int tmp;
+
+    if ( a > b )
+        SWAP(a,b,tmp);
+    if ( b > c )
+        SWAP(b,c,tmp);
+    if ( a > b )
+        SWAP(a,b,tmp);
+
+    if ( a + b <= c )
+        return "Not a Triangle";
+
+    if( a == b && b == c )
+        return "Equilateral";
+    if( a == b && a != c )
+        return "Isosceles";
+    else if( b == c && b != a )
+        return "Isosceles";
+    else
+        return "Scalene";
+}
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%s",triangle_type(a,b,c));
+    return 0;
+}
